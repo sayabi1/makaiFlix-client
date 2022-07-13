6ec16d436021dba2016c436456609bb921c4b143
sayabi1 <95686914+sayabi1@users.noreply.github.com>
2022-07-13 06:18:15 +0200

    imported axios and fetched movies data from the makaiflix database

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import {MovieView} from '../movie-view/movie-view';
 export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [
-                {_id: 1, Titel: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',Genre:'Science-Fiction,Action,Adventure',Director:'Christopher Nolan', ImagePath:'https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392/?ref_=tt_ov_i'},
-                {_id: 2, Titel:'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',Genre:'Drama',Director:'Frank Darabont', ImagePath: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i'},
-                {_id: 3, Titel: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',Genre:'Action,Drama,Romance',Director:'Ridley Scott', ImagePath: 'https://www.imdb.com/title/tt0172495/mediaviewer/rm2442542592/?ref_=tt_ov_i'}
-            ],
-            selectedMovie: null
+            movies: [],
+            selectedMovie: null,
+            user: null
         };
+    }
+    componentDidMount() {
+      axios.get('https://makai-flix-db.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
     }
     setSelectedMovie(newSelectedMovie) {
         this.setState({
@@ -20,16 +31,20 @@ export class MainView extends React.Component {
     }
     render() {
         const { movies, selectedMovie } = this.state;
-    
-    
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+        /* If there is no user, the loginView is rendered . If there is a user looged in, the user details are *passed as a prop to the LoginView*/
+         if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)}/>
+       // Before Movies have been loaded
+        if (movies.length === 0) return <div className="main-view" />;
     
         return (
           <div className="main-view">
-            {selectedMovie
+            { /* if the selected movies is not null , that the selected movie will be returned 
+            , otherwise all *movies will be returned*/
+            selectedMovie
               ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
               : movies.map(movie => (
-                <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+                <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
               ))
             }
           </div>
