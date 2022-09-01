97521f288d3ec926bdaf1b3adee1cb96cd483a3b
sayabi1 <95686914+sayabi1@users.noreply.github.com>
2022-09-01 16:32:33 +0200

    movie-card updated

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom"
 
 export class MovieCard extends React.Component {
     render() {
         const {movie, onMovieClick} = this.props
         return (
             <Card style={{margin:'.75rem'}}>
-                <div style={{maxHeight:'20rem', overflow:'hidden'}}><Card.Img variant="top" src={movie.ImagePath}/></div>
+                <Card.Img variant="top" src={movie.ImagePath}/>
                 <Card.Body>
                     <Card.Title>{movie.Title}</Card.Title>
-                    <Card.Text>{movie.Description}</Card.Text>
-                    <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+                    <Card.Text className='description'>{movie.Description}</Card.Text>
+                    <Card.Footer>
+                        <Link to={`/movies/${movie._id}`}>
+                            <Button className='open' variant="button">See More...</Button>
+                            </Link>
+                    </Card.Footer>
                 </Card.Body>
             </Card>
         );
@@ -25,5 +30,5 @@ MovieCard.prototypes = {
         Description: PropTypes.string.isRequired,
         ImageURL: PropTypes.string.isRequired
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
+    onMovieClick: PropTypes.func,
 };
