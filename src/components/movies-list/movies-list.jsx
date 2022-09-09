5759934a5d6a2860eda75e0cf8806a3f26138187
sayabi1 <95686914+sayabi1@users.noreply.github.com>
2022-09-09 17:20:02 +0200

    movie-list component created

---
+++ src/components/movies-list/movies-list.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import{ MovieCard } from '../movie-card/movie-card';
+import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
+
+const mapStateToProps = state => {
+    const { VisibilityFilter } = state;
+    return { VisibilityFilter };
+};
+
+function MoviesList(props) {
+    const { movies, visibilityFilter } = props;
+    let filteredMovies = movies;
+
+
+
+if (visibilityFilter !== '') {
+    filteredMovies = movies.filter(m => m.Titel.toLowerCase().includes(visibilityFilter.toLowerCase()));
+}
+
+if (!movies) return <div className='main-view'/>;
+
+return (<>
+   <Col md={12} style={{ margin: '1em'}}>
+     <VisibilityFilterInput VisibilityFilter={visibilityFilter} />
+    </Col>
+    {filteredMovies.map(m => (
+        <Col md={3} key={m._id}>
+           <MovieCard movie={m} />
+        </Col>
+    ))}
+</>
+);
+}
+
+MoviesList.propTypes = {
+    movies: PropTypes.arrayOf(
+        PropTypes.shape({
+            filter: PropTypes.func,
+
+        })
+    ),
+    visibilityFilter: PropTypes.string,
+}
+export default connect(mapStateToProps)(MoviesList);
