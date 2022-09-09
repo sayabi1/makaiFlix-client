8949b474d17c4b8f038ba0b4d525d66fc278f13d
sayabi1 <95686914+sayabi1@users.noreply.github.com>
2022-09-09 17:21:28 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,44 @@
-import React  from "react";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Container from "react-bootstrap/Container";
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import moviesApp from "./reducers/reducers";
+import { devToolsEnhancer } from "redux-devtools-extension";
+
+import  MainView  from "./components/main-view/main-view";
+
+//Import statement to indicate tha we need to bundel ./index.scss;
+import "./index.scss";
+
+const store = createStore(moviesApp, devToolsEnhancer());
+
+//Main component (will eventually use all others)
+class MakaiFlixApplication extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Container>
+          <MainView />
+        </Container>
+      </Provider>
+    );
+  }
+}
+
+//Find the Root of our app
+const container = document.getElementsByClassName("app-container") [0];
+
+// Tell React to render our app in the root DOM element
+ReactDOM.render(React.createElement(MakaiFlixApplication),container);
+
+
+/*import React  from "react";
 import ReactDOM  from "react-dom";
 import Container from "react-bootstrap/Container";
+import { createStore } from 'redux';
+
 
 import { MainView } from "./components/main-view/main-view";
 // import statement to indicate thst you need to bundel ìndex.scss`
@@ -21,4 +59,4 @@ class MakaiFlixApplication extends React.Component {
 const container = document.getElementsByClassName('app-container')[0];
 
 // Tells react to render your app in the root DOM element
-ReactDOM.render(React.createElement(MakaiFlixApplication),container);
+ReactDOM.render(React.createElement(MakaiFlixApplication),container);*/
