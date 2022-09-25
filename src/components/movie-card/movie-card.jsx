e2aa3fb661e7b6646eae25b3f51e44895ff9e951
sayabi1 <95686914+sayabi1@users.noreply.github.com>
2022-09-25 13:03:31 +0200

    Add favorite move function and button added

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,8 +3,40 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom"
+import axios from 'axios';
 
 export class MovieCard extends React.Component {
+    addFavorite() {
+        const token = localStorage.getItem("token");
+        const user = localStorage.getItem("user");
+        axios
+          .post(`https://makai-flix-api.herokuapp.com/users/${user}/movies/${this.props.movie._id}`, {}, {
+            headers: { Authorization: 'Bearer ' + token }
+          })
+            .then(response => {
+              alert(`Added to Favorite List`)
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+      };
+    
+      removeFavorite () {
+        const token = localStorage.getItem("token");
+        const user = localStorage.getItem("user");
+        axios
+          .delete(`https://makai-flix-api.herokuapp.com/users/${user}/movies/${this.props.movie._id}`, {
+           headers: {
+              Authorization: 'Bearer ' + token
+            }
+          })
+            .then(response => {
+              alert(`Deleted out of you Favorite List`)
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+      };
     render() {
         const {movie, onMovieClick} = this.props
         return (
@@ -17,6 +49,8 @@ export class MovieCard extends React.Component {
                         <Link to={`/movies/${movie._id}`}>
                             <Button className='open' variant="button">See More...</Button>
                             </Link>
+                            <Button variant="outline-warning" value={movie._id} onClick={(e) => this.addFavorite(e, movie)}>Add to Favourites</Button>
+                            <Button variant="outline-warning" value={movie._id} onClick={(e) => this.removeFavorite(e, movie)}>Delete</Button>
                     </Card.Footer>
                 </Card.Body>
             </Card>
